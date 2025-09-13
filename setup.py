c57807c0f6b51f59b8dd6d225d93cfbb11f3608c
T. Vasanth Chowdary <vasanthchowdarythumati@gmail.com>
2025-09-13 12:32:33 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical Generative AI",
+    version="0.0.0",
+    author="Vasanth Chowdary",
+    author_email='vasanthchowdarythumati@gmail.com',
+    packages=find_packages(),
+    install_requires= []
+
+)
